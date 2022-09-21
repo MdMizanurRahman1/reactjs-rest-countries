1c4f8bbde19c0a310919551b21237a76290a53ac
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2022-09-21 20:09:03 +0200

    mulyiple data

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -18,11 +18,7 @@ const Countries = () => {
             <h1>Hello from Countries: {countries.length}</h1>
 
             {
-                countries.map(country => <Country
-                    name={country.name.common}
-                    population={country.population}
-                    area={country.area}
-                ></Country>)
+                countries.map(country => <Country country={country}></Country>)
             }
 
         </div>
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
+    const { name, area, population, region } = props.country;
     return (
         <div className='country'>
-            <h2>Country: {props.name}</h2>
-            <p>Population: {props.population}</p>
-            <p>Area: {props.area}</p>
+            <h2>Name: {name.common}</h2>
+            <p>Area:{area}</p>
+            <p>Population:{population}</p>
+            <p>Region:{region}</p>
+
         </div>
     );
 };
